32be853b27a950151bf672f836e21292d4b3dd7d
MansiGupta888 <shubhamtalukdar06@gmail.com>
2025-09-25 01:05:30 +0530

    FEAT: Added unimplemented error for api contract

--- src/controllers/problems.controller.js
+++ src/controllers/problems.controller.js
@@ -4,19 +4,31 @@ function pingProblemController(req,res){
     })
 }
 function addProblem(req,res){
+    // res function has a status function it set the status code of the http response and onthe of that you can chain the json
+    return res.status(500).json({
+        message : 'Not Implemented'
+    })
 
 }
 function getProblem(req,res){
-
+    return res.status(500).json({
+        message : 'Not Implemented'
+    })
 }
 function getProblems(req,res){
-
+    return res.status(500).json({
+        message : 'Not Implemented'
+    })
 }
 function deleteProblem(req,res){
-
+    return res.status(500).json({
+        message : 'Not Implemented'
+    })
 }
 function updateProblem(req,res){
-
+    return res.status(500).json({
+        message : 'Not Implemented'
+    })
 }
 module.exports = {
     addProblem,
