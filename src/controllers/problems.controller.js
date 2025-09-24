5e3f62a06e45d5740904b683c7f9823930087fb6
MansiGupta888 <shubhamtalukdar06@gmail.com>
2025-09-25 01:19:52 +0530

    FEAT: change the status code from 500 to 501 and Migrated from numeric status codes to http-status-codes library

--- src/controllers/problems.controller.js
+++ src/controllers/problems.controller.js
@@ -1,3 +1,5 @@
+const {StatusCodes} = require('http-status-codes')
+
 function pingProblemController(req,res){
     return res.json({
         message : "problem controller is up"
@@ -5,28 +7,32 @@ function pingProblemController(req,res){
 }
 function addProblem(req,res){
     // res function has a status function it set the status code of the http response and onthe of that you can chain the json
-    return res.status(500).json({
+    /**
+     * res.status -> it return same response object with status property set 
+     * res.status.json -> it return same response object which has status property set but this json to be returned also set 
+     */
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message : 'Not Implemented'
     })
 
 }
 function getProblem(req,res){
-    return res.status(500).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message : 'Not Implemented'
     })
 }
 function getProblems(req,res){
-    return res.status(500).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message : 'Not Implemented'
     })
 }
 function deleteProblem(req,res){
-    return res.status(500).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message : 'Not Implemented'
     })
 }
 function updateProblem(req,res){
-    return res.status(500).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message : 'Not Implemented'
     })
 }
